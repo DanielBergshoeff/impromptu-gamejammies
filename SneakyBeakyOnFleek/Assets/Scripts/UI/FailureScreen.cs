56683765cd1e99ce52badcdbbc8038ac660a7222
RubenBergshoeff <rubicalme@gmail.com>
2022-06-18 10:44:14 +0200

    Implemented fail screen and reaction

--- SneakyBeakyOnFleek/Assets/Scripts/UI/FailureScreen.cs
+++ SneakyBeakyOnFleek/Assets/Scripts/UI/FailureScreen.cs
@@ -2,22 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FailureScreen : MonoBehaviour
 {
-    [SerializeField] private GameObject failureOverlay = default;
+    [SerializeField] private CanvasGroup failureOverlay = default;
     [SerializeField] private AudioSource audioSource = default;
     [SerializeField] private AudioClip failureSound = default;
+    [SerializeField] private Button retryButton = default;
 
     private void Awake() {
-        failureOverlay.SetActive(false);
+        failureOverlay.gameObject.SetActive(false);
+        failureOverlay.alpha = 0;
         GameEvents.OnPlayerSpotted += HandlePlayerSpotted;
+        retryButton.onClick.AddListener(HandleRetryButtonClick);
     }
 
     private void HandlePlayerSpotted() {
-        failureOverlay.SetActive(true);
+        failureOverlay.gameObject.SetActive(true);
+        failureOverlay.DOFade(1, 0.5f);
         if(failureSound != null) {
             audioSource.PlayOneShot(failureSound);
         }
+    }
+
+    private void HandleRetryButtonClick() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
     }
 }
